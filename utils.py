3978398739be9c8cefe9cdd704ef4fb72a3a0623
lamantinushka <39338519+lamantinushka@users.noreply.github.com>
2018-12-22 18:29:28 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,18 +9,17 @@ def static_w2v(SPPMI, rank):
     return U*np.sqrt(S)
 
 class Corpus(object):
-    def __init__(self, k = 5, path='data'):
+    def __init__(self, k = 5):
         self.SPPMI = []
         self.k = 5
         self.times = 27
         for t in range(self.times):
-            M = sio.loadmat(os.path.join(path, 'pmi_{}.mat'.format(t)))['pmi'] 
+            M = sio.loadmat('data/pmi_{}.mat'.format(t))['pmi'] 
             M.data -= np.log(k)
             M.data = np.array([x if x>0 else 0. for x in M.data])
             M.eliminate_zeros()
             self.SPPMI.append(M)
 
-        df = pd.read_csv(os.path.join(path, 'wordIDHash.csv'),
-            names=['idx', 'word', 'freq'], index_col='word')
+        df = pd.read_csv('data/wordIDHash.csv', names=['idx', 'word', 'freq'], index_col='word')
         self.vocab = df['idx'].to_dict()
         self.size = len(self.vocab)
